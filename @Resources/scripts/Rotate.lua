6a2df8eadd399e58ddcbc0d7ee1df0134dffaf5c
Marco Vockner <marcopixel@live.de>
2017-04-08 00:20:17 +0200

    Cleaned up skin comments

--- @Resources/scripts/Rotate.lua
+++ @Resources/scripts/Rotate.lua
@@ -1,29 +1,26 @@
 -- @author alatsombath / Github: alatsombath
 
 function Initialize()
-  -- Get the config variable for multiple visualizers
+  -- Get the config variable so it won't apply the changes to every rainmeter skin loaded
   config = SKIN:GetVariable("Config")
-  
+
   -- Get nearestAxis variable and set matrix to be a string
   nearestAxis, matrix = SKIN:ParseFormula(SKIN:GetVariable("NearestAxis")), ""
-  
-  -- Get matrix string by sending a bang to the lua file
-  SKIN:Bang("!WriteKeyValue", "Matrix", "OnUpdateAction", '[!CommandMeasure ScriptInitialize """matrix = "[Matrix]"""" "#ROOTCONFIG#\\Settings\\init"]', "#@#include\\MeasureRotate.inc")
-  
+
   -- Set options for each config
   local barHeight = SKIN:ParseFormula(SKIN:GetVariable("BarHeight"))
   local barWidth, barGap = SKIN:ParseFormula(SKIN:GetVariable("BarWidth")) * SKIN:ParseFormula(SKIN:GetVariable("ScaleVisualizer")), SKIN:ParseFormula(SKIN:GetVariable("BarGap")) * SKIN:ParseFormula(SKIN:GetVariable("ScaleVisualizer"))
   local offset = barWidth + barGap
   local angle = SKIN:ParseFormula(SKIN:GetVariable("Angle"))
   local meterName, lowerLimit, upperLimit = {}, 1, SKIN:ParseFormula(SKIN:GetVariable("BarCount"))
-  
+
   -- Add group to MeterBars and then update them
   for i = lowerLimit, upperLimit do
     meterName[i] = "MeterBar" .. i-1
     SKIN:Bang("!SetOption", meterName[i], "Group", "GroupBars", config)
     SKIN:Bang("!UpdateMeter", meterName[i], config)
   end
-  
+
   -- Change Positioning based on NearestAxis
   if nearestAxis ~= 0 then
 	SKIN:Bang("!SetOptionGroup", "GroupBars", "W", barHeight, config)
@@ -38,7 +35,7 @@ function Initialize()
       SKIN:Bang("!SetOption", meterName[i], "X", offset * (i - lowerLimit), config)
     end
   end
-  
+
   if angle > 0 then
 	SKIN:Bang("!SetOptionGroup", "GroupProgressBar", "Hidden", 1, config)
   end
@@ -54,13 +51,13 @@ function Initialize()
     SKIN:Bang("!UpdateMeterGroup", "GroupBars", config)
     SKIN:Bang("!SetOptionGroup", "GroupBars", "TransformationMatrix", "", config)
   end
-  
+
   -- Update the group
   SKIN:Bang("!UpdateMeterGroup", "GroupBars", config)
-  
+
   -- Remove the OnUpdateAction from MeasureRotate.inc to remove log spam
   SKIN:Bang("!WriteKeyValue", "Matrix", "OnUpdateAction", "", "#@#include\\MeasureRotate.inc")
-  
+
   -- Deactivate init skin
   SKIN:Bang("!DeactivateConfig")
 end
