1791350e7f58fe2edb22632442f7ec21f05c2c80
Marco Vockner <marcopixel@live.de>
2017-08-17 00:15:25 +0200

    Added drop shadow option
    This will cast a small drop shadow behind the visualizer and song
    information.

--- @Resources/scripts/Rotate.lua
+++ @Resources/scripts/Rotate.lua
@@ -8,38 +8,47 @@ function Initialize()
   nearestAxis, matrix = SKIN:ParseFormula(SKIN:GetVariable("NearestAxis")), ""
 
   -- Set options for each config
+  local scale = SKIN:ParseFormula(SKIN:GetVariable("ScaleVisualizer"))
   local barHeight = SKIN:ParseFormula(SKIN:GetVariable("BarHeight"))
-  local barWidth = SKIN:ParseFormula(SKIN:GetVariable("BarWidth")) * SKIN:ParseFormula(SKIN:GetVariable("ScaleVisualizer"))
-  local barGap = SKIN:ParseFormula(SKIN:GetVariable("BarGap")) * SKIN:ParseFormula(SKIN:GetVariable("ScaleVisualizer"))
+  local barWidth = SKIN:ParseFormula(SKIN:GetVariable("BarWidth")) * scale
+  local barGap = SKIN:ParseFormula(SKIN:GetVariable("BarGap")) * scale
   local offset = math.floor(barWidth + barGap)
   local angle = SKIN:ParseFormula(SKIN:GetVariable("Angle"))
-  local meterName, lowerLimit, upperLimit = {}, 1, SKIN:ParseFormula(SKIN:GetVariable("BarCount"))
+  local meterName, meterShadow, lowerLimit, upperLimit = {}, {}, 1, SKIN:ParseFormula(SKIN:GetVariable("BarCount"))
 
   -- Add group to MeterBars and then update them
   for i = lowerLimit, upperLimit do
     meterName[i] = "MeterBar" .. i-1
+    meterShadow[i] = "MeterShadowBar" .. i-1
     SKIN:Bang("!SetOption", meterName[i], "Group", "GroupBars | GroupDynamicColors", config)
+    SKIN:Bang("!SetOption", meterShadow[i], "Group", "GroupBars | GroupShadowBars", config)
     SKIN:Bang("!UpdateMeter", meterName[i], config)
+    SKIN:Bang("!UpdateMeter", meterShadow[i], config)
   end
 
   -- Change Positioning based on NearestAxis
   if nearestAxis ~= 0 then
-	SKIN:Bang("!SetOptionGroup", "GroupBars", "W", barHeight, config)
+    SKIN:Bang("!SetOptionGroup", "GroupBars", "W", barHeight, config)
     SKIN:Bang("!SetOptionGroup", "GroupBars", "H", barWidth, config)
+    SKIN:Bang("!SetOptionGroup", "GroupShadowBars", "H", barWidth, config)
     for i = lowerLimit, upperLimit do
       SKIN:Bang("!SetOption", meterName[i], "Y", offset * (i - lowerLimit), config)
+      SKIN:Bang("!SetOption", meterShadow[i], "Y", (4*scale) + offset * (i - lowerLimit), config)
+      SKIN:Bang("!SetOption", meterName[i], "X", (4*scale), config)
     end
   else
     SKIN:Bang("!SetOptionGroup", "GroupBars", "W", barWidth, config)
     SKIN:Bang("!SetOptionGroup", "GroupBars", "H", barHeight, config)
+    SKIN:Bang("!SetOptionGroup", "GroupShadowBars", "H", barHeight + (4*scale), config)
     for i = lowerLimit, upperLimit do
       SKIN:Bang("!SetOption", meterName[i], "X", offset * (i - lowerLimit), config)
+      SKIN:Bang("!SetOption", meterShadow[i], "X", (4*scale) + offset * (i - lowerLimit), config)
     end
   end
-  
-  SKIN:Bang("!SetOption", "Width", "Formula", math.ceil(offset * (upperLimit) - barGap), config)
-  
 
+  SKIN:Bang("!SetOption", "Width", "Formula", math.ceil(offset * (upperLimit) - barGap), config)
+
+  -- disable progress bar when angle is higher than 0
   if angle > 0 then
 	SKIN:Bang("!SetOptionGroup", "GroupProgressBar", "Hidden", 1, config)
   end
@@ -58,6 +67,7 @@ function Initialize()
 
   -- Update the group
   SKIN:Bang("!UpdateMeterGroup", "GroupBars", config)
+  SKIN:Bang("!UpdateMeterGroup", "GroupShadowBars", config)
 
   -- Remove the OnUpdateAction from MeasureRotate.inc to remove log spam
   SKIN:Bang("!WriteKeyValue", "Matrix", "OnUpdateAction", "", "#@#include\\MeasureRotate.inc")
