6e485882a699e73153d89cecd9150e04951c95d5
Brad Charboneau <b.charboneau1@gmail.com>
2023-06-17 12:12:16 -0500

    update comment

--- @Resources/scripts/Rotate.lua
+++ @Resources/scripts/Rotate.lua
@@ -53,7 +53,7 @@ function Initialize()
   -- write skin width to variables file
   SKIN:Bang("!WriteKeyValue", "Variables", "SkinWidth", skinWidth, "#@#variables.ini")
 
-  -- When Angle is 0 revert to default behaviour
+  -- When Angle is 0 or 180 revert to default behaviour
   if angle ~= 0 and angle ~= 180 then
     if nearestAxis ~= 0 then
       SKIN:Bang("!SetOptionGroup", "GroupBars", "BarOrientation", "Horizontal", config)
